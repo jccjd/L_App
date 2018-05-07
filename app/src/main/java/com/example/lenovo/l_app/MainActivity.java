7c3e2ebf9b1a93716d5256ea8a65e6db59edb851
jccjd <1340944538@qq.com>
2018-05-07 23:19:58 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/MainActivity.java
+++ app/src/main/java/com/example/lenovo/l_app/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.lenovo.l_app;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationBar mNavigationBar;
+    private List<Fragment> mFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    private void initView() {
+         mNavigationBar = (BottomNavigationBar) findViewById(R.id.btm_nav_bar);
+
     }
 }
