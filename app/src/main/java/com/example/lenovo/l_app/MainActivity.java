4288b4fdeb68eaec1a9dd29c429369f28c29aed0
jccjd <1340944538@qq.com>
2018-05-07 23:33:02 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/MainActivity.java
+++ app/src/main/java/com/example/lenovo/l_app/MainActivity.java
@@ -4,7 +4,12 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.example.lenovo.l_app.ui.GithubFragment;
+import com.example.lenovo.l_app.ui.HomeFragment;
+import com.example.lenovo.l_app.ui.favoriteFragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +26,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        mFragments = new ArrayList<>();
+        mFragments.add(new HomeFragment());
+        mFragments.add(new GithubFragment());
+        mFragments.add(new favoriteFragment());
+        
+        mNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp,"Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_favorite_white_24dp,"favorite"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_github_circle_white_24dp,"Github"))
+                .setMode(BottomNavigationBar.MODE_FIXED).initialise();
+        mNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
 
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+
+            }
+        });
     }
 
     private void initView() {
