efc18f1fe0b2e4740d6f41f38bb2e6883f01fe15
jccjd <1340944538@qq.com>
2018-05-09 14:44:22 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/MainActivity.java
+++ app/src/main/java/com/example/lenovo/l_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lenovo.l_app;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
@@ -12,7 +14,7 @@ import com.example.lenovo.l_app.ui.favoriteFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener {
 
     private BottomNavigationBar mNavigationBar;
     private List<Fragment> mFragments;
@@ -30,31 +32,39 @@ public class MainActivity extends AppCompatActivity {
         mFragments.add(new HomeFragment());
         mFragments.add(new GithubFragment());
         mFragments.add(new favoriteFragment());
-        
-        mNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp,"Home"))
-                .addItem(new BottomNavigationItem(R.drawable.ic_favorite_white_24dp,"favorite"))
-                .addItem(new BottomNavigationItem(R.drawable.ic_github_circle_white_24dp,"Github"))
+
+        mNavigationBar.addItem(new BottomNavigationItem(R.drawable.ic_home_white_24dp, "Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_favorite_white_24dp, "favorite"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_github_circle_white_24dp, "Github"))
                 .setMode(BottomNavigationBar.MODE_FIXED).initialise();
-        mNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(int position) {
-
-            }
-
-            @Override
-            public void onTabUnselected(int position) {
-
-            }
-
-            @Override
-            public void onTabReselected(int position) {
-
-            }
-        });
+        setFragments(0);
+        mNavigationBar.setTabSelectedListener(this);
+    }
+    private void setFragments(int position) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.main_container,mFragments.get(position));
+        fragmentTransaction.commit();
     }
 
     private void initView() {
-         mNavigationBar = (BottomNavigationBar) findViewById(R.id.btm_nav_bar);
+        mNavigationBar = (BottomNavigationBar) findViewById(R.id.btm_nav_bar);
+
+    }
+
+
+    @Override
+    public void onTabSelected(int position) {
+        setFragments(position);
+    }
+
+    @Override
+    public void onTabUnselected(int position) {
+
+    }
+
+    @Override
+    public void onTabReselected(int position) {
 
     }
 }
