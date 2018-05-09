b75c40f145452a8cd0e646474ab4b72eb82088bf
jccjd <1340944538@qq.com>
2018-05-09 21:51:37 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/ui/HomeFragment.java
+++ app/src/main/java/com/example/lenovo/l_app/ui/HomeFragment.java
@@ -2,7 +2,11 @@ package com.example.lenovo.l_app.ui;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +17,9 @@ import com.example.lenovo.l_app.R;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
-
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private View mView;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -24,7 +29,36 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        initView();
+        init();
+        return mView;
     }
 
+    private void init() {
+        mViewPager.setAdapter(new MyFramentPagerAdapter());
+    }
+
+    private void initView() {
+        mTabLayout = (TabLayout) mView.findViewById(R.id.tab_layout);
+        mViewPager = (ViewPager) mView.findViewById(R.id.View_page);
+
+    }
+
+    private class MyFramentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFramentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 }
