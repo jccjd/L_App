2e86d783de098359f4d34f6146d0130e774d3d92
jccjd <1340944538@qq.com>
2018-05-10 11:27:47 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/net/ApiService.java
+++ app/src/main/java/com/example/lenovo/l_app/net/ApiService.java
@@ -1,5 +1,12 @@
 package com.example.lenovo.l_app.net;
 
-public class ApiService {
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import java.util.List;
+
+public interface ApiService {
+    @GET("getnews.php")
+    Call<HttpResult<List<news>>>
 
 }
