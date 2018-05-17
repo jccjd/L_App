98f608258b9662ff6bd3bda90e060f34d8b7e597
jccjd <1340944538@qq.com>
2018-05-17 21:55:38 +0800

    updata

--- app/src/main/java/com/example/lenovo/l_app/ui/NewsFragment.java
+++ app/src/main/java/com/example/lenovo/l_app/ui/NewsFragment.java
@@ -1,18 +1,30 @@
 package com.example.lenovo.l_app.ui;
 
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.lenovo.l_app.R;
+import com.example.lenovo.l_app.net.News;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private View mView;
+    private List<News> mNews;
 
 
     public NewsFragment() {
@@ -24,7 +36,26 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        mView =  inflater.inflate(R.layout.fragment_news, container, false);
+        initview();
+        init();
+        return mView;
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    
+    private void init() {
+        mNews = new ArrayList<>();
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+    }
+
+
+    private void initview() {
+        mRecyclerView = (RecyclerView) mView.findViewById(R.id.recycler_view);
     }
 
 }
